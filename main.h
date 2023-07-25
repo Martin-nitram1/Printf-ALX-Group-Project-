42b11a377470774ba91c6053a406c718ea33130c
Martin Omondi <134892561+Martin-nitram1@users.noreply.github.com>
2023-07-25 17:28:52 +0300

    Update main.h

--- main.h
+++ main.h
@@ -36,14 +36,15 @@ void print_number(int n);
 int print_unsigned(va_list l, flags_t *f);
 int count_digit(int i);
 
+/* convert */
+char *convert(unsigned long int num, int base, int lowercase);
+
 /* print_bases */
 int print_hex(va_list l, flags_t *f);
 int print_hex_big(va_list l, flags_t *f);
 int print_binary(va_list l, flags_t *f);
 int print_octal(va_list l, flags_t *f);
 
-/* converter */
-char *convert(unsigned long int num, int base, int lowercase);
 
 /* _printf */
 int _printf(const char *format, ...);
