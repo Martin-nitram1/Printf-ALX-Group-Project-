8d5bdce997b78f527ef0624f5579835aaf045824
jgugwa <120091295+jgugwa@users.noreply.github.com>
2023-07-26 22:27:12 +0300

    Update funcs.c

--- funcs.c
+++ funcs.c
@@ -3,7 +3,7 @@
 
 /**
  * _putchar - writes the character c to stdout
- * @c: This The character to print
+ * @c: This is the character to print
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
