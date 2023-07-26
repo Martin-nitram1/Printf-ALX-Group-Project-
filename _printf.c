96b73fc80017b9ac3898e28c16e5dd971d10778d
Martin Omondi <134892561+Martin-nitram1@users.noreply.github.com>
2023-07-26 04:30:08 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -5,9 +5,7 @@
 /**
  * _printf - produces output according to format
  * @format: string containing the characters and the specifiers
- * Description: this function will call the get_print function that will
- * determine which printing function to call depending on the conversion
- * specifiers contained into fmt
+ *
  * Return: length of the formatted output string
  */
 int _printf(const char *format, ...)
