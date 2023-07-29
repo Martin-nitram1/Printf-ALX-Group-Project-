2387168ac9c9d803d95367eaf6e905c0eeea9e34
Martin Omondi <134892561+Martin-nitram1@users.noreply.github.com>
2023-07-29 04:08:52 +0300

    Update print_alpha.c

--- print_alpha.c
+++ print_alpha.c
@@ -2,11 +2,11 @@
 
 /**
  * print_string - loops through a string and prints
- * all characters in it
- * @l: va_list args from _printf
+ * every character
+ * @l: va_list arguments from _printf
  * @f: pointer to the struct flags that determines
  * if a flag is passed to _printf
- * 
+ *
  * Return: number of char printed
  */
 int print_string(va_list l, flags_t *f)
@@ -25,7 +25,7 @@ int print_string(va_list l, flags_t *f)
  * @l: va_list arguments from _printf
  * @f: pointer to the struct flags that determines
  * if a flag is passed to _printf
- * 
+ *
  * Return: number of char printed
  */
 int print_char(va_list l, flags_t *f)
