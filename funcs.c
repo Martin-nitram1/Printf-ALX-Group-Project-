07ce78e1620501ebbab5def8e8ff3795b2b0bd27
Martin Omondi <134892561+Martin-nitram1@users.noreply.github.com>
2023-07-29 04:00:28 +0300

    Update funcs.c

--- funcs.c
+++ funcs.c
@@ -3,10 +3,11 @@
 
 /**
  * _putchar - writes the character c to stdout
- * @c: This is the character to print
- *
+ * @c: The character to print
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
+ * Description: _putchar uses a local buffer of 1024 to call write
+ * as little as possible
  */
 int _putchar(char c)
 {
@@ -29,7 +30,6 @@ int _putchar(char c)
 /**
  * _puts - prints a string to stdout
  * @str: pointer to the string to print
- *
  * Return: number of chars written
  */
 int _puts(char *str)
