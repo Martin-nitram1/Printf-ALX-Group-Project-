4867d9a98ec72d1ded1e50bba33119862ca5c172
Martin Omondi <134892561+Martin-nitram1@users.noreply.github.com>
2023-07-29 04:03:38 +0300

    Update print_custom.c

--- print_custom.c
+++ print_custom.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * print_npc - Non printable characters
+ * print_bigS - Non printable characters
  * (0 < ASCII value < 32 or >= 127) are
  * printed this way: \x, followed by the ASCII code
  * value in hexadecimal (upper case - always 2 characters)
  * @l: va_list arguments from _printf
  * @f: pointer to the struct flags that determines
- * if a flag is passed to _printf
+ * if a flag is passed 
+ *
  * Return: number of char printed
  */
-int print_npc(va_list l, flags_t *f)
+int print_bigS(va_list l, flags_t *f)
 {
 	int i, count = 0;
 	char *res;
@@ -41,7 +42,8 @@ int print_npc(va_list l, flags_t *f)
  * print_rev - prints a string in reverse
  * @l: argument from _printf
  * @f: pointer to the struct flags that determines
- * if a flag is passed to _printf
+ * if a flag is passed
+ *
  * Return: length of the printed string
  */
 int print_rev(va_list l, flags_t *f)
@@ -64,9 +66,10 @@ int print_rev(va_list l, flags_t *f)
 
 /**
  * print_rot13 - prints a string using rot13
- * @l: list of arguments from _printf
+ * @l: list of arguments 
  * @f: pointer to the struct flags that determines
- * if a flag is passed to _printf
+ * if a flag is passed 
+ *
  * Return: length of the printed string
  */
 int print_rot13(va_list l, flags_t *f)
@@ -97,7 +100,8 @@ int print_rot13(va_list l, flags_t *f)
 /**
  * print_percent - prints a percent
  * @l: va_list arguments from _printf
- * @f: pointer to the struct flags in which we turn the flags on
+ * @f: pointer to the struct flags 
+ *
  * Return: number of char printed
  */
 int print_percent(va_list l, flags_t *f)
